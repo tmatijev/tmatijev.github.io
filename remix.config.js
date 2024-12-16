77d1346b8ca18538ec1b459dbcb12d6453d98dbc
tmatijev <matas0412@gmail.com>
2024-12-16 23:40:01 +0100

    update remix

--- remix.config.js
+++ remix.config.js
@@ -1,19 +1,13 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-const config = {
-  appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  serverBuildPath: "build/index.js",
+export default {
+  ignoredRouteFiles: ["**/.*"],
   publicPath: "/tmatijev-github-pages/",
-  exportBuildDirectory: "dist",
+  serverModuleFormat: "esm",
   future: {
     v3_fetcherPersist: true,
     v3_lazyRouteDiscovery: true,
     v3_relativeSplatPath: true,
     v3_singleFetch: true,
     v3_throwAbortReason: true,
-  },
-  serverEnvironmentVariables: ["FORMSPREE_KEY", "RECAPTCHA_SITE_KEY"],
-  browserEnvironmentVariables: ["RECAPTCHA_SITE_KEY"]
+  }
 };
-
-export default config; 
