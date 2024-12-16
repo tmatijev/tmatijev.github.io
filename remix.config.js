1354f516be66bbfe6004d26018fc8b18e2aa6371
tmatijev <matas0412@gmail.com>
2024-12-17 00:18:39 +0100

    React init

--- remix.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-const config = {
-  serverEnvironmentVariables: ["FORMSPREE_KEY", "RECAPTCHA_SITE_KEY"],
-  browserEnvironmentVariables: ["RECAPTCHA_SITE_KEY"]
-};
-
-export default config; 
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Hello GitHub Pages!</h1>
+      <p>This is a test deployment</p>
+    </div>
+  )
+}
+
+export default App 
