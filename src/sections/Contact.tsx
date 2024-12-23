797710311020cadf867d993fd9f4947b5bdc6e1f
tmatijev <matas0412@gmail.com>
2024-12-23 17:18:49 +0100

    Fix form

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -8,7 +8,7 @@ import { config } from "../config";
 
 function ContactForm() {
   const { executeRecaptcha } = useGoogleReCaptcha();
-  const [state, handleSubmit] = useForm(config.formspreeKey);
+  const [state, handleSubmit] = useForm(import.meta.env.VITE_FORMSPREE_KEY);
 
   if (!config.formspreeKey || !config.recaptchaKey) {
     return (
